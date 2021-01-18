8f6b24a86c3baac1d4f1d7341fe82add642f979a
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-18 15:34:26 +0800

    included ImageAdjustment in project tracking

---
+++ app/src/main/java/com/pparreno/cameratrial/ui/photoeditor/ImageAdjustmentActivity.kt
@@ -0,0 +1,13 @@
+package com.pparreno.cameratrial.ui.photoeditor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pparreno.cameratrial.R
+
+class ImageAdjustmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_adjustment)
+    }
+}
