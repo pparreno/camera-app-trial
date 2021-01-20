a25e4fdce46eb654eae93d445d6d4215a639d6f5
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-20 10:57:14 +0800

    commiting changes to app theming and layout of bottombar with bottom navigation

--- app/src/main/java/com/pparreno/cameratrial/MainActivity.kt
+++ app/src/main/java/com/pparreno/cameratrial/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.pparreno.cameratrial
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.pparreno.cameratrial.databinding.ActivityImageAdjustmentBinding
+import com.pparreno.cameratrial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        setSupportActionBar(binding.bottomAppBar)
+
+        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -22,5 +33,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_timeline, R.id.navigation_camera, R.id.navigation_profile))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
     }
 }
