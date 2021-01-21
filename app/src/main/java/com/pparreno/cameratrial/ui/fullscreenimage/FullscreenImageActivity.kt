da8c1117b8fe93bb5c2b5dc074d255b6d5536597
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-21 15:52:47 +0800

    comiting changes to fullscreen activity and newly created custom view class, ZOomImageView

---
+++ app/src/main/java/com/pparreno/cameratrial/ui/fullscreenimage/FullscreenImageActivity.kt
@@ -0,0 +1,12 @@
+package com.pparreno.cameratrial.ui.fullscreenimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pparreno.cameratrial.R
+
+class FullscreenImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullscreen_image)
+    }
+}
